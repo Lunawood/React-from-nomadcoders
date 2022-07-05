37395b00495826be38e4a3517afd8f5b66b00445
Lunawood <whail14@naver.com>
2022-07-05 22:43:45 +0900

    React 22.07.05

--- 01. ReactJS로 영화 웹 서비스 만들기/03.react-cleanup-function/src/App.js
+++ 01. ReactJS로 영화 웹 서비스 만들기/03.react-cleanup-function/src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+
+function Hello() {
+  /*
+  function byFn() {
+    console.log("bye :(")
+  }
+  function hiFn() {
+    console.log("created :)");
+    return byFn;
+  }
+  useEffect(hiFn, []);
+  return (<h1>Hello</h1>);
+  */
+  /*
+  useEffect(function () {
+    console.log("hi :)");
+    return function () {
+      console.log("bye :(")
+    };
+  });
+  */
+  // 이런 함수를 더 많이 씀(더 짧다)
+  useEffect(() => {
+    console.log("hi :)");
+    // 파괴함수는 잘 쓰이지 않는다.
+    // 생성함수에서 "return 파괴함수"를 만들면 된다.
+    return function () {
+      console.log("bye :(")
+    };
+  }, []);
+
+}
 
 function App() {
+  const [showing, setShowing] = useState(false);
+  const onClick = () => setShowing((prev) => !prev);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {showing ? <Hello /> : null}
+      <button onClick={onClick}>{showing ? "Hide" : "Show"}</button>
     </div>
   );
 }
