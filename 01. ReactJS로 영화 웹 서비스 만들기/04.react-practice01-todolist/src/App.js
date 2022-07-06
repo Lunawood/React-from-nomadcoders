17b03dabc10c7b05ef87f41dec54f0f89d08db9d
Lunawood <whail14@naver.com>
2022-07-06 22:16:19 +0900

    React 22.07.06

--- 01. ReactJS로 영화 웹 서비스 만들기/04.react-practice01-todolist/src/App.js
+++ 01. ReactJS로 영화 웹 서비스 만들기/04.react-practice01-todolist/src/App.js
@@ -1,5 +1,34 @@
-function App() {
+import { useState, useEffect } from "react";
 
+function App() {
+    const [toDo, setToDo] = useState("");
+    const [toDos, setToDos] = useState([]);
+    const onChange = (event) => setToDo(event.target.value);
+    const onSubmit = (event) => {
+        event.preventDefault();
+        if (toDo === "") {
+            return;
+        }
+        setToDo("");
+        // setToDos(function (currentArray) { return...});
+        // food = [1,2,3] + 4
+        // [4, ...food] = [4,1,2,3]
+        setToDos(currentArray => [toDo, ...currentArray]);
+    }
+    return (
+        <div>
+            <h1>My To Dos ({toDos.length})</h1>
+            <form onSubmit={onSubmit}>
+                <input
+                    onChange={onChange}
+                    value={toDo}
+                    type="text"
+                    placeholder="Write your to do..."
+                />
+                <button>Add To Do</button>
+            </form>
+        </div>
+    );
 }
 
 export default App;
