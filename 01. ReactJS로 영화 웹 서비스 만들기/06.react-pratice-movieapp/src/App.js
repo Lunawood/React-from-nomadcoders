fc884990f04a85ec3f3f5369c4bf4299b7fbf721
Lunawood <whail14@naver.com>
2022-07-13 23:28:43 +0900

    React Router

--- 01. ReactJS로 영화 웹 서비스 만들기/06.react-pratice-movieapp/src/App.js
+++ 01. ReactJS로 영화 웹 서비스 만들기/06.react-pratice-movieapp/src/App.js
@@ -1,5 +1,18 @@
+import { BrowserRouter as Router, Route, Routes, } from "react-router-dom";
+import Detail from "./routes/Detail";
+import Home from "./routes/Home";
 function App() {
-  return null;
+  // Route는 URL중 페이지를 넘겼을 때 더 붙는 코드를 이야기한다. localhost:3000/(~~~~)
+  // path 에 / 만 붙이면 home page으로 이동한다.
+  return (
+    // react-router-dom@6 을 썼고 Switch(v5) 대신 Routes와 element을 사용
+    <Router >
+      <Routes >
+        <Route path="/" element={<Home />} />
+        <Route path="/movie" element={<Detail />} />
+      </ Routes >
+    </Router>
+  );
 }
 
 export default App;
